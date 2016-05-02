a52463a8249dee88b1d7fc89d8d03e3cd7d63149
Naoya Shibahara <naoya@cps.im.dendai.ac.jp>
2016-05-02 18:27:15 +0900

    bugfix can't click novelview toolbar navigatinoIcon

--- app/src/main/java/net/nashihara/naroureader/activities/NovelViewActivity.java
+++ app/src/main/java/net/nashihara/naroureader/activities/NovelViewActivity.java
@@ -11,15 +11,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.balysv.materialmenu.MaterialMenuDrawable;
 
 import net.nashihara.naroureader.R;
 import net.nashihara.naroureader.RealmUtils;
 import net.nashihara.naroureader.databinding.ActivityNovelViewBinding;
+import net.nashihara.naroureader.dialogs.OkCancelDialogFragment;
 import net.nashihara.naroureader.entities.Novel4Realm;
 import net.nashihara.naroureader.fragments.NovelBodyFragment;
-import net.nashihara.naroureader.dialogs.OkCancelDialogFragment;
 
 import io.realm.Realm;
 import io.realm.RealmQuery;
@@ -44,6 +45,7 @@ public class NovelViewActivity extends AppCompatActivity implements NovelBodyFra
     private String ncode;
     private String writer;
 
+    private int toolBarHeight = 0;
     private int nowPage;
     private SharedPreferences pref;
     private static final String PREF_IS_HIDE = "is_hide";
@@ -171,12 +173,23 @@ public class NovelViewActivity extends AppCompatActivity implements NovelBodyFra
     public void onSingleTapConfirmedAction(boolean isHide) {
         View decor = this.getWindow().getDecorView();
         if (isHide) {
-            decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+            if (toolBarHeight > 0) {
+                ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) binding.novelContainer.getLayoutParams();
+                params.topMargin = toolBarHeight;
+                binding.novelContainer.setLayoutParams(params);
+            }
+
             binding.appBar.setVisibility(View.VISIBLE);
+            decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         }
         else {
-            decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE);
+            toolBarHeight = binding.appBar.getHeight();
+            ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) binding.novelContainer.getLayoutParams();
+            params.topMargin = 0;
+            binding.novelContainer.setLayoutParams(params);
+
             binding.appBar.setVisibility(View.GONE);
+            decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE);
         }
     }
 
