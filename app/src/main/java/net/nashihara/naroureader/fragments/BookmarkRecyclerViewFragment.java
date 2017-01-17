11ebe61637e4ef9c8442188266a48a4b55d7a6a1
Naoya Shibahara <naoya@cps.im.dendai.ac.jp>
2017-01-17 16:12:46 +0900

    Refactor bookmark recycler view fragment

--- app/src/main/java/net/nashihara/naroureader/fragments/BookmarkRecyclerViewFragment.java
+++ app/src/main/java/net/nashihara/naroureader/fragments/BookmarkRecyclerViewFragment.java
@@ -6,7 +6,6 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,7 +14,6 @@ import net.nashihara.naroureader.R;
 import net.nashihara.naroureader.activities.NovelViewActivity;
 import net.nashihara.naroureader.databinding.FragmentSimpleRecycerViewBinding;
 import net.nashihara.naroureader.models.entities.Novel4Realm;
-import net.nashihara.naroureader.utils.OnFragmentReplaceListener;
 import net.nashihara.naroureader.utils.RealmUtils;
 import net.nashihara.naroureader.views.adapters.SimpleRecyclerViewAdapter;
 
@@ -25,13 +23,13 @@ import io.realm.Realm;
 import io.realm.RealmResults;
 
 public class BookmarkRecyclerViewFragment extends Fragment {
-    private static final String TAG = BookmarkRecyclerViewFragment.class.getSimpleName();
 
-    private RecyclerView mRecyclerView;
     private SimpleRecyclerViewAdapter adapter;
-    private OnFragmentReplaceListener mListener;
-    private Context mContext;
+
+    private Context context;
+
     private FragmentSimpleRecycerViewBinding binding;
+
     private ArrayList<Novel4Realm> novels = new ArrayList<>();
 
     public BookmarkRecyclerViewFragment() {}
@@ -50,21 +48,10 @@ public class BookmarkRecyclerViewFragment extends Fragment {
             return;
         }
 
-        adapter = new SimpleRecyclerViewAdapter(mContext);
-        adapter.setOnItemClickListener((view, position) -> {
-            final Novel4Realm novel = novels.get(position);
+        adapter = new SimpleRecyclerViewAdapter(context);
+        adapter.setOnItemClickListener((view, position) -> startNovelActivity(position));
 
-            Intent intent = new Intent(mContext, NovelViewActivity.class);
-            intent.putExtra("ncode", novel.getNcode());
-            intent.putExtra("page", novel.getBookmark());
-            intent.putExtra("title", novel.getTitle());
-            intent.putExtra("writer", novel.getWriter());
-            intent.putExtra("totalPage", novel.getTotalPage());
-
-            startActivity(intent);
-        });
-
-        Realm realm = RealmUtils.getRealm(mContext);
+        Realm realm = RealmUtils.getRealm(context);
         RealmResults<Novel4Realm> results = realm.where(Novel4Realm.class).notEqualTo("bookmark", 0).findAll();
 
         for (Novel4Realm novel4Realm : results) {
@@ -75,14 +62,25 @@ public class BookmarkRecyclerViewFragment extends Fragment {
         adapter.addDataOf(novels);
     }
 
+    private void startNovelActivity(int position) {
+        final Novel4Realm novel = novels.get(position);
+
+        Intent intent = new Intent(context, NovelViewActivity.class);
+        intent.putExtra("ncode", novel.getNcode());
+        intent.putExtra("page", novel.getBookmark());
+        intent.putExtra("title", novel.getTitle());
+        intent.putExtra("writer", novel.getWriter());
+        intent.putExtra("totalPage", novel.getTotalPage());
+
+        startActivity(intent);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_simple_recycer_view, container, false);
 
-        // TODO: RecyclerView
-        mRecyclerView = binding.recycler;
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(mContext));
-        mRecyclerView.setAdapter(adapter);
+        binding.recycler.setLayoutManager(new LinearLayoutManager(context));
+        binding.recycler.setAdapter(adapter);
 
         binding.progressBar.setVisibility(View.GONE);
         binding.recycler.setVisibility(View.VISIBLE);
@@ -93,18 +91,11 @@ public class BookmarkRecyclerViewFragment extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        mContext = context;
-        if (context instanceof OnFragmentReplaceListener) {
-            mListener = (OnFragmentReplaceListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                + " must implement context instanceof OnFragmentReplaceListener");
-        }
+        this.context = context;
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
     }
 }
