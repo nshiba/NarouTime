8457188d8b1e6f2c185faeb04de6675f2ff9c099
Naoya Shibahara <naoya@cps.im.dendai.ac.jp>
2017-01-17 15:51:59 +0900

    Add search controller

--- app/src/main/java/net/nashihara/naroureader/controller/SearchController.java
+++ app/src/main/java/net/nashihara/naroureader/controller/SearchController.java
@@ -1,5 +1,7 @@
 package net.nashihara.naroureader.controller;
 
+import android.renderscript.Int2;
+
 import net.nashihara.naroureader.views.SearchView;
 
 import java.util.ArrayList;
@@ -24,6 +26,47 @@ public class SearchController implements Controller<SearchView> {
         view = null;
     }
 
+    private int validateLimit(String limitStr) {
+        int limit;
+        if (limitStr.equals("")) {
+            limit = 0;
+        } else {
+            limit = Integer.parseInt(limitStr);
+        }
+
+        return limit;
+    }
+
+    private Int2 validateMinMax(String minLength, String maxLength) {
+        int min, max;
+
+        if (minLength.equals("")) {
+            min = 0;
+        } else {
+            min = Integer.parseInt(minLength);
+        }
+
+        if (maxLength.equals("")) {
+            max = 0;
+        } else {
+            max = Integer.parseInt(maxLength);
+        }
+
+        return new Int2(max, min);
+    }
+
+    private ArrayList<Integer> validateGenreIds(boolean[] genreChecked) {
+        NovelGenre[] genreIds = NovelGenre.values();
+        ArrayList<Integer> genres = new ArrayList<>();
+        for (int i = 0; i < genreChecked.length; i++) {
+            if (genreChecked[i]) {
+                genres.add(genreIds[i].getId());
+            }
+        }
+
+        return genres;
+    }
+
     public void shapeSearchQuery(
         String ncode,
         String limitStr,
@@ -42,41 +85,16 @@ public class SearchController implements Controller<SearchView> {
         boolean pickup,
         boolean[] genreChecked) {
 
-        int limit;
-        if (limitStr.equals("")) {
-            limit = 0;
-        } else {
-            limit = Integer.parseInt(limitStr);
-        }
-
+        int limit = validateLimit(limitStr);
         if (limit > 500) {
             view.showError();
             return;
         }
 
-        int min, max;
-
-        if (minLength.equals("")) {
-            min = 0;
-        } else {
-            min = Integer.parseInt(minLength);
-        }
-
-        if (maxLength.equals("")) {
-            max = 0;
-        } else {
-            max = Integer.parseInt(maxLength);
-        }
-
-        NovelGenre[] genreIds = NovelGenre.values();
-        ArrayList<Integer> genres = new ArrayList<>();
-        for (int i = 0; i < genreChecked.length; i++) {
-            if (genreChecked[i]) {
-                genres.add(genreIds[i].getId());
-            }
-        }
+        Int2 minMax = validateMinMax(minLength, maxLength);
+        ArrayList<Integer> genres = validateGenreIds(genreChecked);
 
         view.showResult(ncode, limit, sortOrder, search, notSearch, targetTitle, targetStory,
-            targetKeyword, targetWriter, time, max, min, end, stop, pickup, genres);
+            targetKeyword, targetWriter, time, minMax.x, minMax.y, end, stop, pickup, genres);
     }
 }
