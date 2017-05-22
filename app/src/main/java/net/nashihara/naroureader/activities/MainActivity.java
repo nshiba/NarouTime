9a0956aa7e6cef7cc983cfb9ceee4312e4bed21d
nshiba <naoya.shiba310@gmail.com>
2017-05-22 13:11:01 +0900

    Add import

--- app/src/main/java/net/nashihara/naroureader/activities/MainActivity.java
+++ app/src/main/java/net/nashihara/naroureader/activities/MainActivity.java
@@ -11,11 +11,13 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.balysv.materialmenu.MaterialMenuDrawable;
