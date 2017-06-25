471805175174373e83d11459ea2d44631154e7ae
nshiba <naoya.shiba310@gmail.com>
2017-06-25 15:41:18 +0900

    Convert java to kotlin in DividerItemDecoration

---
+++ app/src/main/java/net/nashihara/naroureader/utils/DividerItemDecoration.kt
@@ -0,0 +1,51 @@
+package net.nashihara.naroureader.utils
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Rect
+import android.graphics.drawable.Drawable
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
+/**
+ * Created by naoya on 15/12/12.
+ * RecyclerViewの行間の線の表示
+ */
+class DividerItemDecoration(context: Context) : RecyclerView.ItemDecoration() {
+
+    private val divider: Drawable
+
+    init {
+        val a = context.obtainStyledAttributes(ATTRS)
+        divider = a.getDrawable(0)
+        a.recycle()
+    }
+
+    override fun onDraw(c: Canvas, parent: RecyclerView, state: RecyclerView.State?) {
+        drawVertical(c, parent)
+    }
+
+    fun drawVertical(c: Canvas, parent: RecyclerView) {
+        val left = parent.paddingLeft
+        val right = parent.width - parent.paddingRight
+
+        val childCount = parent.childCount
+        for (i in 0..childCount - 1) {
+            val child = parent.getChildAt(i)
+            val params = child.layoutParams as RecyclerView.LayoutParams
+            val top = child.bottom + params.bottomMargin
+            val bottom = top + divider.intrinsicHeight
+            divider.setBounds(left, top, right, bottom)
+            divider.draw(c)
+        }
+    }
+
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State?) {
+        outRect.set(0, 0, 0, divider.intrinsicHeight)
+    }
+
+    companion object {
+
+        private val ATTRS = intArrayOf(android.R.attr.listDivider)
+    }
+}
