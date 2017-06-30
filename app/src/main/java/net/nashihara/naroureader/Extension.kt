3df06ce199e2a7aead74977ab958723ffbdc4be5
nshiba <naoya.shiba310@gmail.com>
2017-06-30 15:57:27 +0900

    Add job extension

--- app/src/main/java/net/nashihara/naroureader/Extension.kt
+++ app/src/main/java/net/nashihara/naroureader/Extension.kt
@@ -1,10 +1,7 @@
 package net.nashihara.naroureader
 
-import kotlinx.coroutines.experimental.CommonPool
-import kotlinx.coroutines.experimental.CoroutineScope
-import kotlinx.coroutines.experimental.CoroutineStart
+import kotlinx.coroutines.experimental.*
 import kotlinx.coroutines.experimental.android.UI
-import kotlinx.coroutines.experimental.launch
 import kotlin.coroutines.experimental.CoroutineContext
 
 fun <T> async(context: CoroutineContext = CommonPool, start: CoroutineStart = CoroutineStart.DEFAULT, block: suspend CoroutineScope.() -> T)
@@ -12,3 +9,7 @@ fun <T> async(context: CoroutineContext = CommonPool, start: CoroutineStart = Co
 
 fun ui(start: CoroutineStart = CoroutineStart.DEFAULT, block: suspend CoroutineScope.() -> Unit)
         = launch(UI, start, block)
+
+fun Job.addTo(jobList: MutableList<Job>): Unit {
+    jobList.add(this)
+}
